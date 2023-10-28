9ac87c283d3e4a249f5261c94cc1c4609862ff6d
arcthh <arathd@smu.edu>
2023-10-28 18:28:28 -0500

    need to check for cycles

--- MyGraph.cpp
+++ MyGraph.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "MyGraph.h"
-
+//references
+//https://www.geeksforgeeks.org/kruskals-algorithm-simple-implementation-for-adjacency-matrix/
+//https://www.programiz.com/dsa/graph-adjacency-list
 
 ostream& operator<<(ostream& os, const Link& l)
 {
@@ -109,12 +111,64 @@ vector<Link> Task1(int n, vector<Link>& pipes, MyHelper& helper)
     helper.mst = res;
    return res;
 }
+/*
+this function
+takes in the number of cities (n), the original set of pipes, a newPipe, and the helper object that
+is being passed, and return whether the new pipe will modify the original solution. If it does, the
+pair of return will contains true, follow by the Link that is displaced. Otherwise it will return
+false, and the Link can be anything (it will be ignored) by the main program.
+Notice that in this case, the helper object is passed by value. So you cannot modify it inside the
+function
+
+The second task basically is asking if I add an edge to the graph, will it change the MST?
+Obviously you can rerun the MST algorithm from scratch. However, if you may be able to speed
+up the algorithm if you are willing to store some extra information when you create the MST, you
+may be able to answer the question faster. Also remember the question I brought up in class:
+“Given a tree, if you add one edge, how many cycle can it form?
+*/
 
 pair<bool, Link> Task2(int n, vector<Link>& pipes, Link newPipe, MyHelper helper)
 {
-  Link l1;
-  pair<bool, Link> sol;
-  sol.first = true;
-  sol.second = newPipe;
+   //load up the mst
+   vector<Link> OGmst = helper.mst;
+
+   //ensure  we don't modify
+   vector<Link> pipesT2 = pipes;
+   pipesT2.push_back(newPipe);
+
+//checking
+   // for (const Link &pipe : pipesT2) {
+   //   cout << pipe.v1 << "->" << pipe.v2 << " " << pipe.w << "  ";
+   // }
+
+
+   //new solution
+   vector<Link> sol2;
+
+   //sort in asc order
+   sort(pipesT2.begin(), pipesT2.end());
+
+   // Create a MyGraph object
+   MyGraph graph = MyGraph(n);
+    for (const Link& pipe : pipesT2) {
+        if (graph.addEdge(pipe.v1, pipe.v2, pipe.w)) {
+            sol2.push_back(pipe);
+            // Check if we have added enough edges to form an MST (n-1 edges)
+            if (sol2.size() == n - 1) {
+                break;
+            }
+        }
+    }
+
+   //THIS PART IS NOT WORKING PROPERLY
+
+    // Compare the two MSTs. If it is modified, they are not equal
+    bool isModified = !(OGmst == sol2);
+
+    std::pair<bool, Link> sol;
+    sol.first = isModified;
+    sol.second = (isModified) ? OGmst[0] : Link(); // You can return any Link if not modified
+
+
   return sol;
 }
--- prog2test.cpp
+++ prog2test.cpp
@@ -7,7 +7,7 @@
 
 main()
 {
-   ifstream ifile("test2_1.txt");
+   ifstream ifile("test2_3.txt");
    int n, e;
    vector<float> satcost;
 
@@ -55,5 +55,4 @@ main()
 		}
 	  cout << endl;
 	}
-
 }
